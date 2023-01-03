15b9828e3b86f14f23e1e3541e2aaaa3962522e0
Kamal-pandey03 <kamal.201712@ncit.edu.np>
2023-01-03 23:48:55 +0545

    Lab-6 Qno-1 Done

--- Lab-6/Displaying.java
+++ Lab-6/Displaying.java
@@ -1,12 +1,12 @@
 import java.sql.*;
 import java.util.Scanner;
 
-class Inserting{
+class Displaying{
     public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
     try{
         String url="jdbc:mysql://localhost:3306/kamal";
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Connection cn=DriverManager.getConnection(url,"root","");
         Statement st=cn.createStatement();
         ResultSet rs=st.executeQuery("SELECT * FROM Students");
@@ -14,6 +14,7 @@ class Inserting{
         while(rs.next()){
             System.out.println("Roll no: "+rs.getInt(1)+"\tName: "+rs.getString(2)+"\tFaculty: "+rs.getString(3));
         }
+        cn.close();
     }catch(ClassNotFoundException e){
         System.out.println(e.getMessage());
     }catch(SQLException e){
